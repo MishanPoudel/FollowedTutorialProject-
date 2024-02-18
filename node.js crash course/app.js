455ea5bcf18f73dee21366092354ee17a806b568
Mishan Poudel <poudelmishan2@gmail.com>
2024-02-18 19:23:06 +0000

    view engines

--- node.js crash course/app.js
+++ node.js crash course/app.js
@@ -3,25 +3,31 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs')
+
 // listen for request
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello World</h1>');
-    res.sendFile('./views/index.html', {root: __dirname})
-});
+    const blogs = [
+        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'How to defeat Bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' }
+      ];
+    res.render('index', { title: 'Home', blogs: blogs });
+  });
 
 app.get('/about', (req, res) => {
     // res.send('<h1>About World</h1>');
-    res.sendFile('./views/about.html', {root: __dirname})
+    res.render('about', {title: 'About'})
 });
 
-// redirects
-app.get('/about-me', (req, res) => {
-    res.redirect('/about')
-});
+app.get('/blogs/create',(req,res)=>{
+    res.render('create', {title: 'Create A New Blog'})
+})
 
 // 404 page
 app.use((req, res)=>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404', {title: 'Page Not Found'})
 })
