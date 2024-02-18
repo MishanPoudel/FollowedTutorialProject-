99910dea53287df23f4c06f3d732d3066e6137a7
Mishan Poudel <poudelmishan2@gmail.com>
2024-02-18 18:05:41 +0000

    Express App

---
+++ node.js crash course/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for request
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello World</h1>');
+    res.sendFile('./views/index.html', {root: __dirname})
+});
+
+app.get('/about', (req, res) => {
+    // res.send('<h1>About World</h1>');
+    res.sendFile('./views/about.html', {root: __dirname})
+});
+
+// redirects
+app.get('/about-me', (req, res) => {
+    res.redirect('/about')
+});
+
+// 404 page
+app.use((req, res)=>{
+    res.status(404).sendFile('./views/404.html', {root: __dirname})
+})
